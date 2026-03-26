6ede39ec34ceef778f94173fe79fee3b19fe45bb
ankitgenus <ankit.mathur@genus.in>
2026-03-26 15:53:59 +0530

    add chrome options

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 
 public class BaseTest {
@@ -13,6 +14,14 @@ public class BaseTest {
     public void setUp(){
         System.out.println("BeforeSuite");
         WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        // 🔥 Required for GitHub Actions / Linux
+        options.addArguments("--headless=new");   // run without UI
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
         driver = new ChromeDriver();
     }
 
